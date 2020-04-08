0842fe044b4a3b176f3eedb4f01e962241abf6b0
Elmar Talibzade <elmar.talibzade@gmail.com>
2020-04-09 00:38:29 +0100

    Added invert() macro for Input and Output fields

---
+++ Output/InvertBooleanMacro.cs
@@ -0,0 +1,30 @@
+﻿using HardCodeLab.RockTomate.Core.Data;
+using HardCodeLab.RockTomate.Core.Enums;
+using HardCodeLab.RockTomate.Core.Attributes;
+
+namespace HardCodeLab.RockTomate.Core.Macros
+{
+    [Macro(FieldType.Output, "invert")]
+    public class InvertBooleanOutputMacro : BaseOutputMacro
+    {
+        /// <inheritdoc />
+        protected override MacroParameter[] GetParameters()
+        {
+            return new[]
+            {
+                MacroParameter.Create<string>("Target variable name."),
+                MacroParameter.Create<bool>("Boolean which will be inverted"),
+            };
+        }
+
+        /// <inheritdoc />
+        protected override bool OnVariableUpdate(JobContext context, string variableName, object newValue)
+        {
+            BaseField targetField;
+            GetOrCreateField<bool>(context, variableName, out targetField);
+
+            targetField.SetValue(!(bool)newValue);
+            return true;
+        }
+    }
+}
