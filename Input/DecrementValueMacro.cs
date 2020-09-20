8eda81f2c09ebba558f7b80ec9ce7115d17e0de7
Elmar Talibzade <elmar.talibzade@gmail.com>
2020-09-21 00:50:05 +0100

    Added macros to increment/decrement values

---
+++ Input/DecrementValueMacro.cs
@@ -0,0 +1,27 @@
+﻿using HardCodeLab.RockTomate.Core.Data;
+using HardCodeLab.RockTomate.Core.Enums;
+using HardCodeLab.RockTomate.Core.Attributes;
+
+namespace HardCodeLab.RockTomate.Core.Macros
+{
+    [Macro(FieldType.Input, "decr")]
+    public class DecrementValueMacro : BaseMacro<decimal>
+    {
+        /// <inheritdoc />
+        protected override Parameter[] GetParameters()
+        {
+            return new[]
+            {
+                Parameter.Create<long>("Value that will be decremented."),
+            };
+        }
+
+        /// <inheritdoc />
+        protected override decimal OnInvoke(JobContext context, params object[] args)
+        {
+            var value = GetArg<long>(args, 0);
+
+            return --value;
+        }
+    }
+}
---
+++ Input/IncrementValueMacro.cs
@@ -0,0 +1,27 @@
+﻿using HardCodeLab.RockTomate.Core.Data;
+using HardCodeLab.RockTomate.Core.Enums;
+using HardCodeLab.RockTomate.Core.Attributes;
+
+namespace HardCodeLab.RockTomate.Core.Macros
+{
+    [Macro(FieldType.Input, "incr")]
+    public class IncrementValueMacro : BaseMacro<decimal>
+    {
+        /// <inheritdoc />
+        protected override Parameter[] GetParameters()
+        {
+            return new[]
+            {
+                Parameter.Create<long>("Value that will be incremented."),
+            };
+        }
+
+        /// <inheritdoc />
+        protected override decimal OnInvoke(JobContext context, params object[] args)
+        {
+            var value = GetArg<long>(args, 0);
+
+            return ++value;
+        }
+    }
+}
